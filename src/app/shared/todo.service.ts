5be6e8819f80e6560863986e2e7b538651cb1c29
Kacper Suder <103267851+Kacpre23@users.noreply.github.com>
2023-01-17 15:38:02 +0100

    Nowe funkcjonalnosci + opis
    Dodano mozliwosc usuwania zadania, oraz zmiany statusu

--- src/app/shared/todo.service.ts
+++ src/app/shared/todo.service.ts
@@ -6,15 +6,24 @@ import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/compat
 })
 export class TodoService {
   firestoreCollection : AngularFirestoreCollection; //zapisywanie 
-  constructor(private firestore: AngularFirestore) {  //chodzimy w interakcje z Firebase
+
+  constructor(private firestore: AngularFirestore) { //chodzimy w interakcje z Firebase
     this.firestoreCollection = firestore.collection('todos'); //odniesienie do kolekcji
   }
 
 
-  addTodo(title: string) { //dodanie tytuły gry z pola textowego do bazy
-    this.firestoreCollection.add( { //wypisanie rzeczy dodawanych do bazy danych: tytuł gry, stan wykonania zadania(domyślnie nie)
+  addTodo(title: string){ //dodanie tytuły gry z pola textowego do bazy
+    this.firestoreCollection.add({ //wypisanie rzeczy dodawanych do bazy danych: tytuł gry, stan wykonania zadania(domyślnie nie)
       title,
-      isDone : false 
+      isDone : false
     })
+  }
+
+  updateTodoStatus(id:string, newStatus:boolean){//zmiana statusu
+    this.firestoreCollection.doc(id).update({isDone:newStatus});//odniesienie się do dokumentu wewnatrz kolekcji, poprzez ID
+  }
+
+  deleteTodo(id:string) {//usuwanie zadania
+    this.firestoreCollection.doc(id).delete();
   }
 }
